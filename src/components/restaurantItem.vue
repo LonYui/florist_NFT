6599b8895799a8d5c2552d777e3b8529ef2af176
tsung-han <d5269357812@gmail.com>
2021-10-06 19:11:34 +0800

    feat:restaurantItem component (non)

---
+++ src/components/restaurantItem.vue
@@ -0,0 +1,48 @@
+<template>
+  <ion-item href="/home">
+    <ion-avatar slot="end">
+      <img v-bind:src="imageUrls[0]" alt="抓不到圖片">
+    </ion-avatar>
+    <ion-avatar slot="end">
+      <img v-bind:src="imageUrls[1]" alt="抓不到圖片">
+    </ion-avatar>
+    <ion-avatar slot="end">
+      <img v-bind:src="imageUrls[2]" alt="抓不到圖片">
+    </ion-avatar>
+    <ion-avatar slot="end">
+      <img v-bind:src="imageUrls[3]" alt="抓不到圖片">
+    </ion-avatar>
+    <ion-avatar slot="end">
+      <img v-bind:src="imageUrls[4]" alt="抓不到圖片">
+    </ion-avatar>
+    <ion-label>
+      <h3>{{ 餐廳名稱 }}</h3>
+      <p> {{ 星數目 }}{{ 星星圖案 }}({{ 評論數量 }}){{ 價位 }}</p>
+      <p> {{ 餐廳類型 }}--{{ 地址 }} </p>
+    </ion-label>
+
+
+  </ion-item>
+</template>
+
+<script>
+import {IonItem, IonLabel, IonAvatar} from "@ionic/vue";
+
+export default {
+  components:{IonItem, IonLabel, IonAvatar},
+  name: "restaurantItem",
+  props:[
+    'imageUrls',
+    '餐廳名稱',
+    '星數目',
+    '星星圖案',
+    '評論數量',
+    '價位',
+    '餐廳類型',
+    '地址'],
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/views/Tab1.vue
+++ src/views/Tab1.vue
@@ -13,60 +13,49 @@
         </ion-toolbar>
       </ion-header>
       <ion-list>
-        <ion-item>
-          <ion-avatar slot="end">
-            <img src="https://lh5.googleusercontent.com/p/AF1QipOH7mxD8RBtWKhNmDamgQyE9qq2F7ptpVhzHMyo=w1920-h1080-k-no">
-          </ion-avatar>
-          <ion-avatar slot="end">
-            <img src="https://lh5.googleusercontent.com/p/AF1QipN8J5BUCV7vbAHvlMCuzkS4vuEkZcAF-ibhZXjb=w1920-h1080-k-no">
-          </ion-avatar>
-          <ion-avatar slot="end">
-            <img src="https://lh5.googleusercontent.com/p/AF1QipMZA9P-1aWTk3MCfnwEiFl4lgZ_4zXkT6TPKG4G=w1920-h1080-k-no">
-          </ion-avatar>
-          <ion-avatar slot="end">
-            <img src="https://lh5.googleusercontent.com/p/AF1QipPsb8jCnmxW2VOT-gE9YRIut_ELnLtv_DvFs2yH=w1920-h1080-k-no">
-          </ion-avatar>
-          <ion-avatar slot="end">
-            <img src="https://lh5.googleusercontent.com/p/AF1QipM_XDAWcSaUhT5fjXVME17qDsYIGJgS_IISNKQZ=w1920-h1080-k-no">
-          </ion-avatar>
-
-          <ion-label>
-            <h3>&lt;餐廳名稱&gt;</h3>
-            <p> &lt;星數目&gt;&#9733;&#9733;&#9733;&#9733;&#9734;(&lt;評論數量&gt;)&lt;價位&gt;</p>
-            <p> &lt;餐廳類型&gt;--&lt;地址&gt; </p>
-          </ion-label>
-        </ion-item>
-        <ion-item>
-          <ion-avatar slot="start">
-            <img src="https://lh5.googleusercontent.com/p/AF1QipOH7mxD8RBtWKhNmDamgQyE9qq2F7ptpVhzHMyo=w1920-h1080-k-no">
-          </ion-avatar>
-          <ion-label>
-            <h3>&lt;餐廳名稱&gt;</h3>
-            <p> &lt;星數目&gt;&#9733;&#9733;&#9733;&#9733;&#9734;(&lt;評論數量&gt;)&lt;價位&gt;</p>
-            <p> &lt;餐廳類型&gt;--&lt;地址&gt; </p>
-          </ion-label>
-        </ion-item>
+        <restaurantItem
+            v-for="restaurant in restaurants"
+            :key="restaurant.placeId"
+            :imageUrls="restaurant.imageUrls"
+            :餐廳名稱="restaurant.餐廳名稱"
+            :星數目="restaurant.星數目"
+            :星星圖案="restaurant.星星圖案"
+            :評論數量="restaurant.評論數量"
+            :價位="restaurant.價位"
+            :餐廳類型="restaurant.餐廳類型"
+            :地址="restaurant.地址"
+        ></restaurantItem>
       </ion-list>
     </ion-content>
   </ion-page>
 </template>
 
 <script lang="js">
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonSearchbar, IonList, IonLabel, IonItem } from '@ionic/vue';
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonSearchbar, IonList } from '@ionic/vue';
+import restaurantItem from '@/components/restaurantItem.vue'
 
 export default  {
   name: 'Tab1',
-  components: {  IonHeader, IonToolbar, IonTitle, IonContent, IonPage, IonSearchbar, IonList, IonLabel, IonItem },
+  components: {  IonHeader, IonToolbar, IonTitle, IonContent, IonPage, IonSearchbar, IonList, restaurantItem},
   data(){
     return {
-      imageUrls:[],
-      餐廳名稱:'',
-      星數目:0.0,
-      星星圖案:'&#9734;&#9734;&#9734;&#9734;&#9734',
-      評論數量:'',
-      價位:'', //$$$
-      餐廳類型:'中餐館',
-      地址:'',
+      restaurants:[{
+        placeId:'ChIJr6Q-A42sQjQRnF0-UERYNto',
+        imageUrls:[
+          "https://lh5.googleusercontent.com/p/AF1QipOH7mxD8RBtWKhNmDamgQyE9qq2F7ptpVhzHMyo=w1920-h1080-k-no",
+          "https://lh5.googleusercontent.com/p/AF1QipN8J5BUCV7vbAHvlMCuzkS4vuEkZcAF-ibhZXjb=w1920-h1080-k-no",
+          "https://lh5.googleusercontent.com/p/AF1QipMZA9P-1aWTk3MCfnwEiFl4lgZ_4zXkT6TPKG4G=w1920-h1080-k-no",
+          "https://lh5.googleusercontent.com/p/AF1QipPsb8jCnmxW2VOT-gE9YRIut_ELnLtv_DvFs2yH=w1920-h1080-k-no",
+          "https://lh5.googleusercontent.com/p/AF1QipM_XDAWcSaUhT5fjXVME17qDsYIGJgS_IISNKQZ=w1920-h1080-k-no"
+        ],
+        餐廳名稱:'北平烤鴨莊',
+        星數目:4.0,
+        星星圖案:'★★★★☆',
+        評論數量:64,
+        價位:'', //$$$
+        餐廳類型:'中餐館',
+        地址:'114台灣台北市內湖區成功路四段20巷7號',
+      }],
     }
   },
 
