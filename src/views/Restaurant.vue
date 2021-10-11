b33475a67a9e96aa9298f90341f73624f3351cb0
tsung-han <d5269357812@gmail.com>
2021-10-11 21:16:49 +0800

    feat:restaurant detail page ui compoment Overview change title to  {{title}} change the slide pixel change image use if make icon feature

--- src/views/Restaurant.vue
+++ src/views/Restaurant.vue
@@ -2,21 +2,20 @@
   <ion-page>
     <ion-header>
       <ion-toolbar>
-        <ion-title>Restaurant</ion-title>
+        <ion-title>{{ 餐廳名稱 }}</ion-title>
       </ion-toolbar>
     </ion-header>
     <ion-content :fullscreen="true">
       <ion-header collapse="condense">
         <ion-toolbar>
-          <ion-title size="large">Restaurant</ion-title>
+          <ion-title size="large">{{ 餐廳名稱 }}</ion-title>
         </ion-toolbar>
       </ion-header>
-      <h3>{{ 餐廳名稱 }}</h3>
       <p> {{ 星數目 }}{{ 星星圖案 }}({{ 評論數量 }})</p>
       <p> {{ 餐廳類型 }}•{{ 價位 }}•{{ 交通時間 }}</p>
       <p> {{ 是否營業中 }}•{{ 營業結束時間或下次開啟時間 }}</p>
       <ion-slides :options="slideOpts">
-        <ion-slide style="width: 150px; height: 200px; border: 2px solid #f8f8f8">
+        <ion-slide style="width: 100px; height: 50px; ">
           <ion-col>
             <ion-chip>
               <ion-icon :icon="arrowRedoOutline"></ion-icon>
@@ -24,42 +23,45 @@
             </ion-chip>
           </ion-col>
         </ion-slide>
-        <ion-slide style="width: 150px; height: 200px; border: 2px solid #f8f8f8">
+        <ion-slide style="width: 100px; height: 50px; ">
           <ion-col>
             <ion-chip>
-              <ion-icon :icon="arrowRedoOutline"></ion-icon>
+              <ion-icon :icon="arrowUpOutline"></ion-icon>
               <ion-label>開始</ion-label>
             </ion-chip>
           </ion-col>
         </ion-slide>
-        <ion-slide style="width: 150px; height: 200px; border: 2px solid #f8f8f8">
+        <ion-slide style="width: 100px; height: 50px; ">
           <ion-col>
             <ion-chip>
-              <ion-icon :icon="arrowRedoOutline"></ion-icon>
+              <ion-icon :icon="callOutline"></ion-icon>
               <ion-label>致電</ion-label>
             </ion-chip>
           </ion-col>
         </ion-slide>
-        <ion-slide style="width: 150px; height: 200px; border: 2px solid #f8f8f8">
+        <ion-slide style="width: 100px; height: 50px; ">
           <ion-col>
             <ion-chip>
-              <ion-icon :icon="arrowRedoOutline"></ion-icon>
+              <ion-icon :icon="saveOutline"></ion-icon>
               <ion-label>儲存</ion-label>
             </ion-chip>
           </ion-col>
         </ion-slide>
-        <ion-slide style="width: 150px; height: 200px; border: 2px solid #f8f8f8">
+        <ion-slide style="width: 100px; height: 50px; ">
           <ion-col>
             <ion-chip>
-              <ion-icon :icon="arrowRedoOutline"></ion-icon>
+              <ion-icon :icon="shareOutline"></ion-icon>
               <ion-label>分享</ion-label>
             </ion-chip>
           </ion-col>
         </ion-slide>
-        <ion-slide style="width: 150px; height: 200px; border: 2px solid #f8f8f8">
+        <ion-slide
+            style="width: 100px; height: 50px; "
+            v-if="官方網站"
+        >
           <ion-col>
             <ion-chip>
-              <ion-icon :icon="arrowRedoOutline"></ion-icon>
+              <ion-icon :icon="earthOutline"></ion-icon>
               <ion-label>網站</ion-label>
             </ion-chip>
           </ion-col>
@@ -67,51 +69,63 @@
       </ion-slides>
 
       <ion-slides :options="slideOpts">
-        <ion-slide style="width: 150px; height: 200px; border: 2px solid #f8f8f8">
+        <ion-slide style="width: 200px; height: 200px; border: 2px solid #f8f8f8"
+                   v-for="imageUrl in imageUrls"
+                   v-bind:key="imageUrl"
+        >
           <ion-col>
-            <ion-label>Card #0</ion-label>
             <img style="pointer-events:none"
-                 v-bind:src="imageUrls[0]" alt="抓不到圖片">
-          </ion-col>
-        </ion-slide>
-        <ion-slide style="width: 150px; height: 200px; border: 2px solid #f8f8f8">
-          <ion-col>
-            <ion-label>Card #1</ion-label>
-            <img style="pointer-events:none"
-                 v-bind:src="imageUrls[1]" alt="抓不到圖片">
-          </ion-col>
-        </ion-slide>
-        <ion-slide style="width: 150px; height: 200px; border: 2px solid #f8f8f8">
-          <ion-col>
-            <ion-label>Card #2</ion-label>
-            <img style="pointer-events:none"
-                 v-bind:src="imageUrls[2]" alt="抓不到圖片">
-          </ion-col>
-        </ion-slide>
-        <ion-slide style="width: 150px; height: 200px; border: 2px solid #f8f8f8">
-          <ion-col>
-            <ion-label>Card #3</ion-label>
-            <img style="pointer-events:none"
-                 v-bind:src="imageUrls[3]" alt="抓不到圖片">
+                 v-bind:src="imageUrl" alt="抓不到圖片">
           </ion-col>
         </ion-slide>
       </ion-slides>
+      <ion-toolbar>
+        <ion-segment value="總覽">
+          <ion-segment-button value="總覽">
+            <ion-label>總覽</ion-label>
+          </ion-segment-button>
+          <ion-segment-button value="最新動態">
+            <ion-label>最新動態</ion-label>
+          </ion-segment-button>
+          <ion-segment-button value="菜單">
+            <ion-label>菜單</ion-label>
+          </ion-segment-button>
+          <ion-segment-button value="評論">
+            <ion-label>評論</ion-label>
+          </ion-segment-button>
+          <ion-segment-button value="相片">
+            <ion-label>相片</ion-label>
+          </ion-segment-button>
+          <ion-segment-button value="關於">
+            <ion-label>關於</ion-label>
+          </ion-segment-button>
+        </ion-segment>
+      </ion-toolbar>
       <!--      tab 總覽-->
-
+      <TabOverview :即時通訊url="即時通訊url" :地址="地址" :外送url="外送url" :官方網站="官方網站" :定位url="定位url" :營業時間="營業時間" :電話="電話" :reviewsDistribution="reviewsDistribution" :totalScore="totalScore" />
     </ion-content>
   </ion-page>
 </template>
 
 <script>
 import {
-  IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonSlides, IonSlide
+  IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonSlides, IonSlide,IonSegmentButton, IonSegment
 } from '@ionic/vue';
-import {arrowRedoOutline, ellipse} from "ionicons/icons";
+import {
+  arrowRedoOutline,
+  arrowUpOutline, callOutline,
+  earthOutline,
+  ellipse,
+  saveOutline,
+  shareOutline
+} from "ionicons/icons";
+import TabOverview from "../components/Restaurant/TabOverview";
 
 export default {
   name: 'Restaurant',
   components: {
-    IonHeader, IonToolbar, IonTitle, IonContent, IonPage, IonSlides, IonSlide
+    TabOverview,
+    IonHeader, IonToolbar, IonTitle, IonContent, IonPage, IonSlides, IonSlide, IonSegmentButton, IonSegment
   },
   data() {
     return {
@@ -121,19 +135,40 @@ export default {
         "https://lh5.googleusercontent.com/p/AF1QipN8J5BUCV7vbAHvlMCuzkS4vuEkZcAF-ibhZXjb=w1920-h1080-k-no",
         "https://lh5.googleusercontent.com/p/AF1QipMZA9P-1aWTk3MCfnwEiFl4lgZ_4zXkT6TPKG4G=w1920-h1080-k-no",
         "https://lh5.googleusercontent.com/p/AF1QipPsb8jCnmxW2VOT-gE9YRIut_ELnLtv_DvFs2yH=w1920-h1080-k-no",
+          "https://lh5.googleusercontent.com/p/AF1QipM_XDAWcSaUhT5fjXVME17qDsYIGJgS_IISNKQZ=w1920-h1080-k-no",
+          "https://lh5.googleusercontent.com/p/AF1QipOCeTs2A4BDU8BhE8cWOmu52DkXisLtv1yr7q_H=w1920-h1080-k-no",
+          "https://lh5.googleusercontent.com/p/AF1QipP0L1Y22bby7PnjXRJXmWp04bNGRI3C0i4etRAl=w1920-h1080-k-no",
+          'https://lh5.googleusercontent.com/p/AF1QipMVyObgCkTauEQZpRcnTJE8nH3ywDfXttGM75WC=w1920-h1080-k-no',
+          "https://lh5.googleusercontent.com/p/AF1QipMEKq59inRKi3-p9N7--C2gdWr3iRR1EY-OutVq=w1920-h1080-k-no",
+          "https://lh5.googleusercontent.com/p/AF1QipNe-UbxZdn0Q9IGussC35Eir9ru_FhindtlQ8Ip=w1920-h1080-k-no",
       ],
       餐廳名稱: '北平烤鴨莊',
       星數目: 4.0,
       星星圖案: '★★★★☆',
       評論數量: 64,
-      價位: '', //$$$
+      價位: '', //(爬不到)
       餐廳類型: '中餐館',
       地址: '114台灣台北市內湖區成功路四段20巷7號',
 
 
-      交通時間: '🛵4分鐘',
-      是否營業中: '營業中',
-      營業結束時間或下次開啟時間: '結束營業時間：下午 9:00',
+      交通時間: '🛵4分鐘',//(爬不到)
+      是否營業中: '營業中',//(爬不到)
+      營業結束時間或下次開啟時間: '結束營業時間：下午 9:00',//(爬不到)
+
+      即時通訊url:null,//(爬不到)
+      外送url:null,//(爬不到)
+      官方網站:null,
+      定位url:null,//(爬不到)
+      營業時間:null,//(爬不到)
+      電話:'+886 2 2790 7077',
+      reviewsDistribution:{
+        oneStar:2,
+        twoStar:5,
+        threeStar:9,
+        fourStar:23,
+        fiveStar:25,
+      },
+      totalScore:4.0,
 
 
     }
@@ -141,7 +176,7 @@ export default {
   setup() {
     const slideOpts = {slidesPerView: 'auto', zoom: false, grabCursor: true};
     return {
-      arrowRedoOutline, slideOpts, ellipse
+      arrowRedoOutline, slideOpts, ellipse,arrowUpOutline,saveOutline,shareOutline,earthOutline, callOutline
     }
   },
   methods: {},
