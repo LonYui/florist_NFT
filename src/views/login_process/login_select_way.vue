65047582540b6f1474d278b5dc7041d23ce10dad
yosontw21 <66150719+yosontw21@users.noreply.github.com>
2021-12-07 01:07:00 +0800

    page1-修改跑排-字型

--- src/views/login_process/login_select_way.vue
+++ src/views/login_process/login_select_way.vue
@@ -1,12 +1,11 @@
 <template>
-	<div class="banner-img">
-	
+		<div class="banner-img">
+			<div class=""></div>  <!-- 這行排版用區塊 不要刪除!! -->
 			<button class="fb-btn">
 				<img src="../imgs/fb-icon.svg" class="fb-icon" />
 				使用Facebook登入
 			</button>
-
-	</div>
+		</div>
 </template>
 
 <script>
@@ -16,6 +15,5 @@
 		name: "login_select_way",
 	};
 </script>
-
-<style scoped>
+	<style scoped>
 </style>
