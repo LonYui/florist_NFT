8e29d623951fde6801052d18af5fc8ec3c3aeb54
tsung-han <d5269357812@gmail.com>
2022-02-03 15:53:15 +0800

    feat:layout sale detail (non)

--- src/views/Sale_detail.vue
+++ src/views/Sale_detail.vue
@@ -1,18 +1,30 @@
 <template>
+  <IonPage>
+    <IonHeader>
+    </IonHeader>
+    <IonContent>
 {{title}}
   <br/>
-{{contract_name}}#{{token_id}}:{{price}}
+{{contract_name}}#{{token_id}}:{{price}}      <IonButton @click="purchase()">purchase</IonButton>
   <br/>
   {{description}}
+
+    </IonContent>
+  </IonPage>
 
 </template>
 
 <script>
+import {IonHeader, IonContent, IonPage, IonButton,} from '@ionic/vue';
+// import {tappaySDK} from "@/mixins/TPD_javascript_sdk"
 const default_sale = {
   title:'ccb 發行 ntf',
   description:'rare dog',
   price:12000,
 }
+// const default_nft  = {
+//   // todo
+// }
 export default {
   name: "Sale_detail",
   data(){
@@ -21,9 +33,20 @@ export default {
       price:default_sale.price,
       description:default_sale.description,
       contract_name:'yolo cat',
+      // card
+      card_number:'',
+      //sdk
+      TPDirect:{}
     }
   },
   props:['token_id'],
+  components: {IonHeader, IonContent, IonPage, IonButton,},
+  // mixins:[tappaySDK,],
+  methods: {
+    purchase(){
+      // TODO redirect to payment page
+    }
+  },
   mounted(){
     // fetch the sale from our app
     var requestOptions = {
@@ -37,7 +60,12 @@ export default {
             this[key] = json[key]
           }
         }))
-    //fetch the contract from ether scan
+    // TODO fetch the contract from ether scan block by nft api
+    // init TPD js sdk
+    window.TPDirect.setupSDK(123166, 'partner_2ASaDPKoiVGntvKotGblzgrrqolDyNHXCXkuAohWEoLJ1aSmsaa7hlVS', 'sandbox')
+    (function (){
+      this.TPDirect = window.TPDirect
+    })
   },
 }
 </script>
