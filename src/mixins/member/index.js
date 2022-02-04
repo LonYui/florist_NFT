ecc0c0273cd75357f5d9d074e8d1752149d47724
tsung-han <d5269357812@gmail.com>
2022-02-04 20:05:27 +0800

    fix: tab3 scan memeber - style memeber mixin - data use member obj - delete userless default data user name - from: response.status_code to: response.status - from:Array.keys to:object.keys(Array)

--- src/mixins/member/index.js
+++ src/mixins/member/index.js
@@ -1,19 +1,23 @@
 export let use_member = {
-    data () {
+    data() {
         return {
-            // mob:'0912345678',
-            sexuality:'m',
-            username:'test',
-            birth_datetime:'1234/12/23',
-            mob_country_code:'+86',
-            email:'test@test.com',
-            image_url:'https://test.com',
-            created_time:'1234/12/23',
+            member: {
+                mob:'0912345678',
+                sexuality: 'm',
+                username: 'test',
+                birth_datetime: '1234/12/23',
+                mob_country_code: '+86',
+                email: 'test@test.com',
+                image_url: 'https://test.com',
+                created_time: '1234/12/23',
+                metamask_address:'0x1234123412341234123412341234',
+            }
+
         }
     },
     methods: {
-        fetch_get_member(uuid){
-            
+        fetch_get_member(uuid) {
+
             var requestOptions = {
                 method: 'GET',
                 redirect: 'follow'
@@ -21,17 +25,26 @@ export let use_member = {
 
             return fetch(`https://${process.env.VUE_APP_ccb_rock_backed_domain}/member/read?member_id=${uuid}`, requestOptions)
         },
-        fetch_post_member({member_id,mob,sexuality,username,birth_datetime,mob_country_code,email,image_url}={}){
+        fetch_post_member({
+                              member_id,
+                              mob,
+                              sexuality,
+                              username,
+                              birth_datetime,
+                              mob_country_code,
+                              email,
+                              image_url
+                          } = {}) {
 
             var formdata = new FormData();
             formdata.append("member_id", member_id);
             formdata.append("mob", mob);
-            if(sexuality) formdata.append("sexuality", sexuality);
-            if(username) formdata.append("username", username);
-            if(birth_datetime) formdata.append("birth_datetime", birth_datetime);
-            if(mob_country_code) formdata.append("mob_country_code", mob_country_code);
-            if(email) formdata.append("email", email);
-            if(image_url) formdata.append("image_url", image_url);
+            if (sexuality) formdata.append("sexuality", sexuality);
+            if (username) formdata.append("username", username);
+            if (birth_datetime) formdata.append("birth_datetime", birth_datetime);
+            if (mob_country_code) formdata.append("mob_country_code", mob_country_code);
+            if (email) formdata.append("email", email);
+            if (image_url) formdata.append("image_url", image_url);
 
             var requestOptions = {
                 method: 'POST',
