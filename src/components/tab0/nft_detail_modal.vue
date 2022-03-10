8d3bae6741d9c7b5bc226c002c9db29b641cb51b
tsung-han <d5269357812@gmail.com>
2022-03-10 19:43:23 +0800

    fix:nft animation open modal (non)

---
+++ src/components/tab0/nft_detail_modal.vue
@@ -0,0 +1,40 @@
+<template>
+<IonHeader>
+  <IonToolbar>
+    <IonButton @click="refresh()">
+      refresh
+    </IonButton>
+  </IonToolbar>
+</IonHeader>
+  <IonContent>
+    <IonRow>
+      <IonCol>
+    <IonImg style="pointer-events:none"
+            :src="image_url" alt="抓不到圖片" />
+      </IonCol>
+    </IonRow>
+    <IonRow>
+      <IonCol>
+        <h3>{{NFT['title']}}</h3>
+        <p>{{NFT['description']}}</p>
+      </IonCol>
+    </IonRow>
+    <a v-bind:href="`https://testnets.opensea.io/assets/${this.address}/${this.token_id_ten}`">https://testnets.opensea.io/assets/{{address}}/{{token_id_ten}}</a>
+  </IonContent>
+</template>
+
+<script>
+import { IonHeader,IonToolbar,IonContent,IonImg,IonRow,IonCol, IonButton } from '@ionic/vue';
+import {use_NFT} from "../../mixins/NFT"
+export default {
+  name: "nft_detail_modal",
+  mixins:[use_NFT],
+  components:{ IonHeader,IonToolbar,IonContent,IonImg,IonRow,IonCol,IonButton },
+  props:['address','token_id','token_type'],
+
+}
+</script>
+
+<style scoped>
+
+</style>
