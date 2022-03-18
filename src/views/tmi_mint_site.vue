7ba62bafd50e8eff2f434d1eebfeb0d6b70a4017
tsung-han <d5269357812@gmail.com>
2022-03-19 00:56:20 +0800

    feat: - countdown - toolbar with linketree - teams use metamask avatar - random 13 img random num

--- src/views/tmi_mint_site.vue
+++ src/views/tmi_mint_site.vue
@@ -1,4 +1,26 @@
 <template>
+  <IonHeader style="--min-height: 20% !important;	font-size: 80%;
+	vertical-align: middle;">
+    <IonToolbar >
+      <IonRow class="ion-justify-content-end">
+        <IonCol size="1">
+      <IonIcon size="large" :icon="logoInstagram"></IonIcon>
+        </IonCol>
+        <IonCol size="1">
+      <IonIcon size="large" :icon="logoDiscord"></IonIcon>
+        </IonCol>
+        <IonCol size="1">
+      <IonIcon size="large" :icon="logoTwitter"></IonIcon>
+        </IonCol >
+        <IonCol size="1">
+      opensea
+        </IonCol>
+      </IonRow>
+      <IonTitle></IonTitle>
+    </IonToolbar>
+  </IonHeader>
+  <IonContent>
+
   <h1>TMI</h1>
   <h2>too much Irene </h2>
   <h2>gallery</h2>
@@ -13,6 +35,10 @@
       </ion-col>
     </ion-slide>
   </ion-slides>
+  <vue-countdown :time="Math.abs(new Date('4/5/2022') - Date.now())" v-slot="{ days, hours, minutes, seconds }">
+    mint date：{{'4/5/2022'}}
+    Time Remaining：{{ days }} days, {{ hours }} hours, {{ minutes }} minutes, {{ seconds }} seconds.
+  </vue-countdown>
   <button @click="mint()" v-show="metamask_response.current?true:false">mint</button>
   <br/>
   <button @click="white_list_mint()" v-show="white_list.includes(metamask_response.current)">white_list_mint</button>
@@ -24,15 +50,36 @@
   <div v-else>
     <button @click="connet_to_metamask()">connet_to_metamask</button>
   </div>
+  <h2>team</h2>
+  <IonRow class="ion-justify-content-center">
+    <IonCol size="4">
+      <metamask_avatar :username="teams['jeffrey']['username']"
+                       :img_url="teams['jeffrey']['image_url']"
+                       :member_balance="null"
+                       :metamask_address="teams['jeffrey']['metamask_address']"
+      />
+    </IonCol>
+    <IonCol size="4">
+      <metamask_avatar :username="teams['kevin']['username']"
+                       :img_url="teams['kevin']['image_url']"
+                       :member_balance="null"
+                       :metamask_address="teams['kevin']['metamask_address']" />
+    </IonCol>
+  </IonRow>
+  </IonContent>
+
 </template>
 
 <script>
 import {loadingController,
-IonSlides,IonSlide,IonImg,IonLabel} from '@ionic/vue';
-
+  IonSlides,IonSlide,IonImg,IonLabel,IonRow,IonCol,IonHeader,IonContent,IonToolbar, IonTitle,IonIcon} from '@ionic/vue';
+import VueCountdown from '@chenfengyuan/vue-countdown';
+import metamask_avatar from '@/components/tab3/metamask_avatar';
+import {logoTwitter,logoInstagram,logoDiscord} from 'ionicons/icons';
 export default {
   name: "tmi_mint_site",
-  components:{IonSlides,IonSlide,IonImg,IonLabel},
+  components:{IonSlides,IonSlide,IonImg,IonLabel,IonRow,IonCol,IonHeader,IonContent,IonToolbar, IonTitle,IonIcon,
+    VueCountdown,metamask_avatar},
   data() {
     return {
       imgs:[
@@ -56,6 +103,10 @@ export default {
       contract_address: '0x000000000',
       white_list: [],
       token_id: '123',
+      teams:{
+        'kevin':{username:'kevin',image_url:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQd0H0AOijnTE9n6eWb2Cxqm9LOMJSLpA_NHw&usqp=CAU',metamask_address:'admin.ccb.eth'},
+        'jeffrey':{username:'jeffrey',image_url:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ-U1ZM7J9dnweXdxds19sBW3Vh5uTR-mt6MQ&usqp=CAU',metamask_address:'jeffrey.eth'}
+      }
     }
   },
   methods: {
@@ -120,7 +171,12 @@ export default {
   },
   mounted() {
     this.white_list = this.abi_get_contract_whitelist()
-  }
+  },
+  setup() {
+    return {
+      logoTwitter,logoInstagram,logoDiscord
+    }
+  },
 }
 </script>
 
