be8210e885b6adcd3c7f7a2c9eb851342822ab29
tsung-han <d5269357812@gmail.com>
2022-03-19 19:09:32 +0800

    feat:about (non)

--- src/views/tmi_mint_site.vue
+++ src/views/tmi_mint_site.vue
@@ -1,8 +1,26 @@
 <template>
-  <IonHeader style="--min-height: 20% !important;	font-size: 80%;
-	vertical-align: middle;">
+  <IonHeader style="--min-height: 20% !important;	font-size: 80%;vertical-align: middle;"
+             class="ion-text-center"
+  >
+    <h1>TMI Lion</h1>
     <IonToolbar >
-      <IonRow class="ion-justify-content-end">
+      <IonRow class="ion-justify-content-start">
+          <IonCol size="1">
+            about
+          </IonCol>
+          <IonCol size="1">
+            roadmap
+          </IonCol>
+          <IonCol size="1">
+            partner&artist
+          </IonCol>
+          <IonCol size="1">
+            gallery
+          </IonCol>
+          <IonCol size="1">
+            team
+          </IonCol>
+
         <IonCol size="1">
       <IonIcon size="large" :icon="logoInstagram"></IonIcon>
         </IonCol>
@@ -19,11 +37,55 @@
       <IonTitle></IonTitle>
     </IonToolbar>
   </IonHeader>
-  <IonContent>
-
-  <h1>TMI</h1>
-  <h2>too much Irene </h2>
-  <h2>gallery</h2>
+  <IonContent style="--ion-background-color:black; --ion-text-color: white ;
+--ion-font-family: 'Noto Sans TC';
+"
+              class="ion-text-center"
+  >
+    <div name="mint">
+    <IonButton @click="mint()" v-show="metamask_response.current?true:false">mint</IonButton>
+    <IonButton @click="white_list_mint()" v-show="white_list.includes(metamask_response.current)">white_list_mint</IonButton>
+    <div v-if="metamask_response.current?true:false">
+      <IonText color="danger">
+      <h1 v-if="metamask_response.network!=='mainnet'"> warn:this is testnet{{ metamask_response.network }}</h1>
+      </IonText>
+      <p>current connect to :{{ metamask_response.current }}</p>
+    </div>
+    <div v-else>
+      <IonButton @click="connet_to_metamask()">connet_to_metamask</IonButton>
+    </div>
+  <vue-countdown :time="Math.abs(new Date('4/5/2022') - Date.now())" v-slot="{ days, hours, minutes, seconds }">
+    mint date：{{'4/5/2022'}}
+    Time Remaining：{{ days }} days, {{ hours }} hours, {{ minutes }} minutes, {{ seconds }} seconds.
+  </vue-countdown>
+    </div>
+    <div name="about" style="  --background: none;
+  background: #847E7E80;
+  background-image: url('https://s3.us-east-2.amazonaws.com/asset.roarrr.io/Lion_logo2+1.png');
+  background-position: center center;
+  background-repeat: no-repeat;
+  background-size: 54vw;
+">
+      <h2 style="font-size: 300%;" class="ion-text-uppercase">about</h2>
+      <br/>
+      <h3 style="font-size: 180%;">當科技與藝術相遇，超越現實的收藏價值。</h3>
+      <br/>
+      <p>TMI Lion 是一個來自臺灣的團隊</p>
+      <p>TMI是Too Much Information的縮寫，目標是帶給大家最多元的內容</p>
+      <p>並從台灣出發接軌國際，</p>
+      <p>本次NFT就是臺灣知名服裝設計師潘怡良搭配全球票選紐約十大刺青店的經營者</p>
+      <p>Mikhail andersson合作發行，並邀請三大影帝- 李康生，溫昇豪，黃河，與兩大國際才女DJ Rayray，共襄盛舉，成為第一批持有者</p>
+      <br/>
+      <p>之後每個月都會舉辦不一樣的線下活動，並持續邀請明星藝人參與，也會將部分收益捐給公益團體，</p>
+      <p>希望從藝術及時尚開始，打造一個超越所有人想像，最多元化的項目</p>
+      <br/>
+      <h4 style="font-size: 150%;">團隊願景</h4>
+      <h4 style="font-size: 150%;">線上結合線下、虛擬串接真實，讓更多人共襄盛舉</h4>
+      <br/>
+      <br/>
+    </div>
+    <div name="gallery">
+    <h2>gallery</h2>
 <!--  4:3-->
   <ion-slides :options="{ slidesPerView: 'auto', zoom: false, grabCursor: true }">
     <ion-slide  style="width: 12.5%; height: 16.6%;
@@ -35,21 +97,8 @@
       </ion-col>
     </ion-slide>
   </ion-slides>
-  <vue-countdown :time="Math.abs(new Date('4/5/2022') - Date.now())" v-slot="{ days, hours, minutes, seconds }">
-    mint date：{{'4/5/2022'}}
-    Time Remaining：{{ days }} days, {{ hours }} hours, {{ minutes }} minutes, {{ seconds }} seconds.
-  </vue-countdown>
-  <button @click="mint()" v-show="metamask_response.current?true:false">mint</button>
-  <br/>
-  <button @click="white_list_mint()" v-show="white_list.includes(metamask_response.current)">white_list_mint</button>
-  <br/>
-  <div v-if="metamask_response.current?true:false">
-    <h1 v-if="metamask_response.network!=='mainnet'"> warn:this is testnet{{ metamask_response.network }}</h1>
-    <p>current connect to :{{ metamask_response.current }}</p>
-  </div>
-  <div v-else>
-    <button @click="connet_to_metamask()">connet_to_metamask</button>
-  </div>
+    </div>
+    <div name="team">
   <h2>team</h2>
   <IonRow class="ion-justify-content-center">
     <IonCol size="4">
@@ -66,19 +115,20 @@
                        :metamask_address="teams['kevin']['metamask_address']" />
     </IonCol>
   </IonRow>
+    </div>
   </IonContent>
 
 </template>
 
 <script>
 import {loadingController,
-  IonSlides,IonSlide,IonImg,IonLabel,IonRow,IonCol,IonHeader,IonContent,IonToolbar, IonTitle,IonIcon} from '@ionic/vue';
+  IonSlides,IonSlide,IonImg,IonLabel,IonRow,IonCol,IonHeader,IonContent,IonToolbar, IonTitle,IonIcon,IonButton,IonText,} from '@ionic/vue';
 import VueCountdown from '@chenfengyuan/vue-countdown';
 import metamask_avatar from '@/components/tab3/metamask_avatar';
 import {logoTwitter,logoInstagram,logoDiscord} from 'ionicons/icons';
 export default {
   name: "tmi_mint_site",
-  components:{IonSlides,IonSlide,IonImg,IonLabel,IonRow,IonCol,IonHeader,IonContent,IonToolbar, IonTitle,IonIcon,
+  components:{IonSlides,IonSlide,IonImg,IonLabel,IonRow,IonCol,IonHeader,IonContent,IonToolbar, IonTitle,IonIcon,IonButton,IonText,
     VueCountdown,metamask_avatar},
   data() {
     return {
@@ -181,5 +231,4 @@ export default {
 </script>
 
 <style scoped>
-
 </style>
