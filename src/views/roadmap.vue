b6d873471f45d3d41e45f0796643e18e37ddbd22
tsung-han <d5269357812@gmail.com>
2022-04-28 01:53:59 +0800

    feat:roadmap (vision and roadmap) (non)

--- src/views/roadmap.vue
+++ src/views/roadmap.vue
@@ -1,9 +1,16 @@
 <template>
-  <div id="roadmap"></div>
+  <div id="roadmap" class="ion-text-center">
+    <IonText color="light"><h3><br/>Vision & Mission</h3></IonText>
+    <IonText color="light"><p style="font-family: 'pixel ch'"> <br/>花花讓世界更可愛</p></IonText>
+    <IonText color="light"><p style="font-family: 'pixel ch'"> <br/>情人節多一點浪漫，母親節多一點溫馨，聖誕節多一點喜氣</p></IonText>
+    <IonText color="light"><p style="font-family: 'pixel ch'"> <br/>Flower is best gift</p></IonText>
+    <IonText color="light"><p style="font-family: 'pixel ch'"> <br/>瞭解植物知識、種植到開花、花語。</p></IonText>
+    <br/><button @click="push_to('/mint')">go to mine</button>
+  </div>
 </template>
 
 <script>
-
+import {IonText} from "@ionic/vue"
 const [s1,s2,cl,
   g1,g2,g3,
   r1,r2,r3,
@@ -22,6 +29,7 @@ const color_stard_for = {
 }
 export default {
   name: "tmi_mint_site",
+  components:{IonText},
   mounted(){
     const sketch = p => {
       let gird = [
@@ -98,10 +106,26 @@ export default {
     }
     const P5 = require('p5')
     new P5(sketch)
-  }
+  },
+  methods:{
+    push_to(path){
+      this.$router.push(path).then(() => {window.location.reload()})
+    }
+  },
 }
 </script>
 
 <style scoped>
+IonText {
+  white-space: pre-line;
+}
+p{
+  background-color: rgba(0,31,220,0.87) ;
+  display: inline;
+}
+h3{
+  background-color: rgba(0,31,220,0.87) ;
+  display: inline;
+}
 
 </style>
